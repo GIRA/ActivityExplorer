018e58f9ed90680749a2e2745f1004521b1d815a
katiaperchet <katiamperchet@gmail.com>
2020-04-30 00:38:42 -0300

    Added atributes to Activity class and deleted ActivitiesController bc it was empty

--- ApiMendieta/Models/Activity.cs
+++ ApiMendieta/Models/Activity.cs
@@ -11,16 +11,19 @@ namespace ApiABMCActivity.Models
 		{
 		}
 		public string Id { get; set; }
+		public string Author { get; set; }
 		public string Area { get; set; }
-		public string Titulo { get; set; }
-		public string Descripcion { get; set; }
-		public string Duracion { get; set; }
+		public string Title { get; set; }
+		public string Institution { get; set; }
+		public int AgeMin { get; set; }
+		public int AgeMax { get; set; }
+		public int ShortDescp { get; set; }
+		public string Description{ get; set; }
+		public string Files { get; set; }
+		public string Duration { get; set; }
+		public string Naps { get; set; }
+		public DateTime PublishingDate { get; set; }
+		public DateTime ModifiedDate { get; set; }
 
 	}
 }
-//public string Institucion { get; set; }
-//public string Creador { get; set; }
-//public string Materiales { get; set; }
-//public string Resumen { get; set; }		
-//public string Pautas { get; set; }
-//public string Naps { get; set; }
