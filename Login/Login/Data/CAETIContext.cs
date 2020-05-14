1ca037a0bcf0384fe3bd1c85ca67e7453d191c13
IanBuceta <iansbrizza@gmail.com>
2020-05-14 18:41:06 -0300

    Now inherits from IdentityDbContext

--- Login/Login/Data/CAETIContext.cs
+++ Login/Login/Data/CAETIContext.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Login.Data
 {
-    public partial class CAETIContext : DbContext
+    public partial class CAETIContext : IdentityDbContext<SystemUser, SystemUserRole, string>
     {
         public CAETIContext()
         {
+
         }
 
         public CAETIContext(DbContextOptions<CAETIContext> options)
@@ -15,8 +17,8 @@ namespace Login.Data
         {
         }
 
-        public virtual DbSet<Login> Login { get; set; }
         public virtual DbSet<SystemUser> SystemUser { get; set; }
+        public virtual DbSet<SystemUserRole> SystemUserRoles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -28,90 +30,7 @@ namespace Login.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Login>(entity =>
-            {
-                entity.HasKey(e => e.LoginEmail);
-
-                entity.Property(e => e.LoginEmail)
-                    .HasColumnName("Login_Email")
-                    .HasMaxLength(320)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.LoginPassword)
-                    .IsRequired()
-                    .HasColumnName("Login_Password")
-                    .HasMaxLength(10)
-                    .IsFixedLength();
-
-                entity.Property(e => e.LoginUserId).HasColumnName("Login_UserId");
-
-                entity.HasOne(d => d.LoginUser)
-                    .WithMany(p => p.Login)
-                    .HasForeignKey(d => d.LoginUserId)
-                    .HasConstraintName("FK_Login_User");
-            });
-
-            modelBuilder.Entity<SystemUser>(entity =>
-            {
-                entity.Property(e => e.SystemUserId)
-                    .HasColumnName("SystemUser_Id")
-                    .ValueGeneratedNever();
-
-                entity.Property(e => e.SystemUserCity)
-                    .IsRequired()
-                    .HasColumnName("SystemUser_City")
-                    .HasMaxLength(50)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.SystemUserDni)
-                    .HasColumnName("SystemUser_Dni")
-                    .HasMaxLength(50)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.SystemUserEmail)
-                    .IsRequired()
-                    .HasColumnName("SystemUser_Email")
-                    .HasMaxLength(320)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.SystemUserImage)
-                    .HasColumnName("SystemUser_Image")
-                    .HasMaxLength(50)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.SystemUserInstitution)
-                    .HasColumnName("SystemUser_Institution")
-                    .HasMaxLength(50)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.SystemUserLastName)
-                    .IsRequired()
-                    .HasColumnName("SystemUser_LastName")
-                    .HasMaxLength(50)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.SystemUserName)
-                    .IsRequired()
-                    .HasColumnName("SystemUser_Name")
-                    .HasMaxLength(50)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.SystemUserPassword)
-                    .IsRequired()
-                    .HasColumnName("SystemUser_Password")
-                    .HasMaxLength(50)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.SystemUserPhone)
-                    .IsRequired()
-                    .HasColumnName("SystemUser_Phone")
-                    .HasMaxLength(50)
-                    .IsUnicode(false);
-            });
-
-            OnModelCreatingPartial(modelBuilder);
+            base.OnModelCreating(modelBuilder);
         }
-
-        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
