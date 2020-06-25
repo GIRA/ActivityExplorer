cbbd4ad39c2b094ee2fedbb83abd4a1b01484781
IanBuceta <iansbrizza@gmail.com>
2020-06-25 19:50:42 -0300

    Added JsonIgnore to Password property

--- BackendMendieta/Model/RegisterViewModel.cs
+++ BackendMendieta/Model/RegisterViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Login.Models
@@ -60,6 +61,7 @@ namespace Login.Models
         
         [Display(Name = "Password")]
         [DataType(DataType.Password)]
+        [JsonIgnore]
         public string Password { get; set; }
     }
 }
